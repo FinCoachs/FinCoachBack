dcac612d611619cfb98d78fa8850a034998b9c84
Eunock <izumishinishi7@gmail.com>
2026-06-10 12:01:18 +0100

    feat(DTO): mise en place des DTOs pour la creation de compte

--- app/DTOs/Comptes/CreateCompteDTO.php
+++ app/DTOs/Comptes/CreateCompteDTO.php
@@ -2,10 +2,18 @@
 
     namespace App\DTOs\Comptes;
 
-
+use App\Http\Requests\Compte\CompteRequest;
 
     class CreateCompteDTO{
         public function __construct(
-            
+            public string $libelle,
+            public string $numero
         ){}
+
+        public static function fromRequest(CompteRequest $request): self{
+            return new self(
+                $request->libelle,
+                $request->numero
+            );
+        }
     }
